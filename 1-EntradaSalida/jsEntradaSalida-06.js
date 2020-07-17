d5b7c9dd1bdd6f51c9e4390f13c7bb74a33d30d5
antonella amichetti <anturruca@hotmail.com>
2020-07-16 23:31:23 -0300

    clase 2

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var vNumeroUno;
+	var vNumeroDos;
+	var vResultado;
+	vNumeroUno = parseInt (document.getElementById("txtIdNumeroUno").value);
+	vNumeroDos = parseInt (document.getElementById("txtIdNumeroDos").value);
+	vResultado = vNumeroUno+vNumeroDos;
+
+	alert ("La suma es "+vResultado);
+
+	/*alert("ok");*/
 }
 
